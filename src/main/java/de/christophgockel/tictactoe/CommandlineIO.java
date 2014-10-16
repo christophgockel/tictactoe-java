13b5d01b15b4491d5038528e7b4a23ecbcd027b0
Christoph Gockel <github@christophgockel.de>
2014-10-16 10:33:14 +0100

    Add more tests for CommandlineIO

--- src/main/java/de/christophgockel/tictactoe/CommandlineIO.java
+++ src/main/java/de/christophgockel/tictactoe/CommandlineIO.java
@@ -1,27 +1,50 @@
 package de.christophgockel.tictactoe;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.List;
 
 public class CommandlineIO implements Output, Input {
+  private InputStream input;
   private PrintStream output;
 
-  public CommandlineIO(PrintStream output) {
+  public CommandlineIO(InputStream input, PrintStream output) {
+    this.input = input;
     this.output = output;
   }
 
   @Override
   public int getMove() {
-    return 0;
+    output.print("Next move: ");
+    try {
+      return input.read();
+    } catch (IOException e) {
+      return 0;
+    }
   }
 
   @Override
   public void show(Board board) {
+    List<Mark> cells = board.getCells();
+    String template = " 1 | 2 | 3 " + "\n" +
+                      " 4 | 5 | 6 " + "\n" +
+                      " 7 | 8 | 9 ";
 
+    for (int i = 0; i < cells.size(); i++) {
+      Mark cell = cells.get(i);
+
+      if (cell != null) {
+        template = template.replace(Integer.toString(i + 1), cell.toString());
+      }
+    }
+
+    output.print(template);
   }
 
   @Override
   public void showWinner(Mark mark) {
-
+    output.println("Winner: " + mark.toString());
   }
 
   @Override
--- src/test/java/de/christophgockel/tictactoe/CommandlineIOTest.java
+++ src/test/java/de/christophgockel/tictactoe/CommandlineIOTest.java
@@ -3,20 +3,23 @@ package de.christophgockel.tictactoe;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.*;
 
 import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class CommandlineIOTest {
   private CommandlineIO io;
   private ByteArrayOutputStream output;
+  private DummyInputStream input;
 
   @Before
-  public void setup() {
+  public void setup() throws IOException {
+    input = new DummyInputStream();
     output = new ByteArrayOutputStream();
-    io = new CommandlineIO(new PrintStream(output));
+
+    io = new CommandlineIO(input, new PrintStream(output));
   }
 
   @Test
@@ -26,7 +29,68 @@ public class CommandlineIOTest {
     assertThat(stdout(), containsString("Game ended in a draw."));
   }
 
+  @Test
+  public void printsWinnerX() {
+    io.showWinner(Mark.X);
+
+    assertThat(stdout(), containsString("Winner: X"));
+  }
+
+  @Test
+  public void printsWinnerO() {
+    io.showWinner(Mark.O);
+
+    assertThat(stdout(), containsString("Winner: O"));
+  }
+
+  @Test
+  public void printsEmptyBoard() {
+    Board board = new Board();
+
+    io.show(board);
+
+    assertThat(stdout(), containsString("1 | 2 | 3"));
+    assertThat(stdout(), containsString("4 | 5 | 6"));
+    assertThat(stdout(), containsString("7 | 8 | 9"));
+  }
+
+  @Test
+  public void printsBoardWithContent() {
+    Board board = new Board();
+    board.setMove(1, Mark.X);
+    board.setMove(5, Mark.O);
+    board.setMove(9, Mark.X);
+
+    io.show(board);
+
+    assertThat(stdout(), containsString("X | 2 | 3"));
+    assertThat(stdout(), containsString("4 | O | 6"));
+    assertThat(stdout(), containsString("7 | 8 | X"));
+  }
+
+  @Test
+  public void asksForNextMove() {
+    io.getMove();
+
+    assertThat(stdout(), containsString("Next move:"));
+  }
+
+  @Test
+  public void getsNextMoveFromStdin() throws IOException {
+    input.valueToBeRead = 42;
+    assertEquals(42, io.getMove());
+  }
+
   private String stdout() {
     return output.toString();
+  }
+
+  private class DummyInputStream extends InputStream {
+    public int valueToBeRead;
+
+    @Override
+    public int read() throws IOException {
+      return valueToBeRead;
+    }
   }
 }
