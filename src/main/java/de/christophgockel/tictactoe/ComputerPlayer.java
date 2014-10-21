b3f73c534f0194ef7e18e6ff6b51e2d6e4909950
Christoph Gockel <github@christophgockel.de>
2014-10-21 20:08:08 +0100

    ComputerPlayer uses Negamax now.

--- src/main/java/de/christophgockel/tictactoe/ComputerPlayer.java
+++ src/main/java/de/christophgockel/tictactoe/ComputerPlayer.java
@@ -9,11 +9,68 @@ public class ComputerPlayer implements Player {
 
   @Override
   public Board nextMove(Board board) {
-    return board.setMove(1, mark);
+    int move = bestMove(board);
+    return board.setMove(move, mark);
+  }
+
+  private int bestMove(Board board) {
+    RatedMove move = negamax(board, -1.0, 1.0, mark);
+    return move.move;
   }
 
   @Override
   public Mark getMark() {
     return mark;
+  }
+
+  private RatedMove negamax(Board board, double alpha, double beta, Mark mark) {
+    int bestMove = -1;
+    double bestScore = -1;
+
+    if (!board.isPlayable()) {
+      return new RatedMove(score(board, mark), bestMove);
+    }
+
+    for (int move : board.getFreeLocations()) {
+      double score = -negamax(board.setMove(move, mark), -beta, -alpha, mark.getOpponent()).score;
+
+      if (score > bestScore) {
+        bestScore = score;
+        bestMove = move;
+      }
+
+      if (score > alpha) {
+        alpha = score;
+      }
+
+      if (alpha >= beta) {
+        break;
+      }
+    }
+
+    return new RatedMove(bestScore, bestMove);
+  }
+
+  private double score(Board board, Mark mark) {
+    int movesMade = 9 - board.getFreeLocations().size();
+
+    if (board.hasWinner()) {
+      if (board.getWinner() == mark) {
+        return 1.0 / movesMade;
+      } else {
+        return -1.0 / movesMade;
+      }
+    }
+    return 0.0;
+  }
+
+  private class RatedMove {
+    public final double score;
+    public final int move;
+
+    public RatedMove(double score, int move) {
+      this.score = score;
+      this.move = move;
+    }
   }
 }
--- src/test/java/de/christophgockel/tictactoe/ComputerPlayerTest.java
+++ src/test/java/de/christophgockel/tictactoe/ComputerPlayerTest.java
@@ -27,15 +27,100 @@ public class ComputerPlayerTest {
   }
 
   @Test
-  public void blocksWinningMoveInRow() {
+  public void blocksWinningMovesInRows() {
     assertThat(boardWithOpponentMovesAt(2, 3), receivesMove(1));
+    assertThat(boardWithOpponentMovesAt(1, 3), receivesMove(2));
+    assertThat(boardWithOpponentMovesAt(1, 2), receivesMove(3));
+
+    assertThat(boardWithOpponentMovesAt(5, 6), receivesMove(4));
+    assertThat(boardWithOpponentMovesAt(4, 6), receivesMove(5));
+    assertThat(boardWithOpponentMovesAt(4, 5), receivesMove(6));
+
+    assertThat(boardWithOpponentMovesAt(8, 9), receivesMove(7));
+    assertThat(boardWithOpponentMovesAt(7, 9), receivesMove(8));
+    assertThat(boardWithOpponentMovesAt(7, 8), receivesMove(9));
+  }
+
+  @Test
+  public void blocksWinningMovesInColumns() {
+    assertThat(boardWithOpponentMovesAt(4, 7), receivesMove(1));
+    assertThat(boardWithOpponentMovesAt(1, 7), receivesMove(4));
+    assertThat(boardWithOpponentMovesAt(1, 4), receivesMove(7));
+
+    assertThat(boardWithOpponentMovesAt(5, 8), receivesMove(2));
+    assertThat(boardWithOpponentMovesAt(2, 8), receivesMove(5));
+    assertThat(boardWithOpponentMovesAt(2, 5), receivesMove(8));
+
+    assertThat(boardWithOpponentMovesAt(6, 9), receivesMove(3));
+    assertThat(boardWithOpponentMovesAt(3, 9), receivesMove(6));
+    assertThat(boardWithOpponentMovesAt(3, 6), receivesMove(9));
+  }
+
+  @Test
+  public void blocksWinningMovesInDiagonals() {
+    assertThat(boardWithOpponentMovesAt(5, 9), receivesMove(1));
+    assertThat(boardWithOpponentMovesAt(1, 9), receivesMove(5));
+    assertThat(boardWithOpponentMovesAt(1, 5), receivesMove(9));
+
+    assertThat(boardWithOpponentMovesAt(5, 7), receivesMove(3));
+    assertThat(boardWithOpponentMovesAt(3, 7), receivesMove(5));
+    assertThat(boardWithOpponentMovesAt(3, 5), receivesMove(7));
+  }
+
+  @Test
+  public void finishesWinningRows() {
+    assertThat(boardWithComputerMovesAt(2, 3), receivesMove(1));
+    assertThat(boardWithComputerMovesAt(1, 3), receivesMove(2));
+    assertThat(boardWithComputerMovesAt(1, 2), receivesMove(3));
+
+    assertThat(boardWithComputerMovesAt(5, 6), receivesMove(4));
+    assertThat(boardWithComputerMovesAt(4, 6), receivesMove(5));
+    assertThat(boardWithComputerMovesAt(4, 5), receivesMove(6));
+
+    assertThat(boardWithComputerMovesAt(8, 9), receivesMove(7));
+    assertThat(boardWithComputerMovesAt(7, 9), receivesMove(8));
+    assertThat(boardWithComputerMovesAt(7, 8), receivesMove(9));
+  }
+
+  @Test
+  public void finishesWinningColumns() {
+    assertThat(boardWithComputerMovesAt(4, 7), receivesMove(1));
+    assertThat(boardWithComputerMovesAt(1, 7), receivesMove(4));
+    assertThat(boardWithComputerMovesAt(1, 4), receivesMove(7));
+
+    assertThat(boardWithComputerMovesAt(5, 8), receivesMove(2));
+    assertThat(boardWithComputerMovesAt(2, 8), receivesMove(5));
+    assertThat(boardWithComputerMovesAt(2, 5), receivesMove(8));
+
+    assertThat(boardWithComputerMovesAt(6, 9), receivesMove(3));
+    assertThat(boardWithComputerMovesAt(3, 9), receivesMove(6));
+    assertThat(boardWithComputerMovesAt(3, 6), receivesMove(9));
+  }
+
+  @Test
+  public void finishesWinningDiagonals() {
+    assertThat(boardWithComputerMovesAt(5, 9), receivesMove(1));
+    assertThat(boardWithComputerMovesAt(1, 9), receivesMove(5));
+    assertThat(boardWithComputerMovesAt(1, 5), receivesMove(9));
+
+    assertThat(boardWithComputerMovesAt(5, 7), receivesMove(3));
+    assertThat(boardWithComputerMovesAt(3, 7), receivesMove(5));
+    assertThat(boardWithComputerMovesAt(3, 5), receivesMove(7));
   }
 
   private Board boardWithOpponentMovesAt(int... moves) {
+    return boardWithMoves(mark.getOpponent(), moves);
+  }
+
+  private Board boardWithComputerMovesAt(int... moves) {
+    return boardWithMoves(mark, moves);
+  }
+
+  private Board boardWithMoves(Mark mark, int[] moves) {
     Board board = new Board();
 
     for (int move : moves) {
-      board = board.setMove(move, mark.getOpponent());
+      board = board.setMove(move, mark);
     }
 
     return computer.nextMove(board);
